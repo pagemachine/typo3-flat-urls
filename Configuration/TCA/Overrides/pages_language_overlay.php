b8b7e2613450af1faae02dd7598830c16b423cd6
Mathias Brodala <mbrodala@pagemachine.de>
2017-10-27 16:08:21 +0200

    [TASK] Only use PathSegmentEvaluator

--- Configuration/TCA/Overrides/pages_language_overlay.php
+++ Configuration/TCA/Overrides/pages_language_overlay.php
@@ -3,8 +3,4 @@ defined('TYPO3_MODE') or die();
 
 $GLOBALS['TCA']['pages_language_overlay']['columns']['tx_realurl_pathsegment']['label'] = 'LLL:EXT:flat_urls/Resources/Private/Language/locallang_db.xlf:pages.tx_realurl_pathsegment';
 $GLOBALS['TCA']['pages_language_overlay']['columns']['tx_realurl_pathsegment']['config']['readOnly'] = true;
-$GLOBALS['TCA']['pages_language_overlay']['columns']['tx_realurl_pathsegment']['config']['eval'] = str_replace(
-    'nospace',
-    \Pagemachine\FlatUrls\Evaluation\SpaceToDashEvaluator::class,
-    $GLOBALS['TCA']['pages_language_overlay']['columns']['tx_realurl_pathsegment']['config']['eval']
-);
+$GLOBALS['TCA']['pages_language_overlay']['columns']['tx_realurl_pathsegment']['config']['eval'] = \Pagemachine\FlatUrls\Evaluation\PathSegmentEvaluator::class;
