c78eef0e4a391fc83fd2ed213bbfadea357feddf
Mathias Brodala <mbrodala@pagemachine.de>
2017-11-16 17:04:35 +0100

    [TASK] Simplify language service setup for tests

--- Tests/Functional/PageTest.php
+++ Tests/Functional/PageTest.php
@@ -8,7 +8,6 @@ namespace Pagemachine\FlatUrls\Functional;
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Lang\LanguageService;
 
 /**
  * Testcase for Page processing
@@ -44,8 +43,8 @@ class PageTest extends FunctionalTestCase
     {
         parent::setUp();
 
-        $GLOBALS['LANG'] = GeneralUtility::makeInstance(LanguageService::class);
-        $GLOBALS['LANG']->init(null);
+        $this->setUpBackendUserFromFixture(1);
+        \TYPO3\CMS\Core\Core\Bootstrap::getInstance()->initializeLanguageObject();
     }
 
     /**
@@ -54,7 +53,6 @@ class PageTest extends FunctionalTestCase
     public function setsPathSegmentOfPages()
     {
         $this->importDataSet(__DIR__ . '/Fixtures/Database/pages.xml');
-        $this->setUpBackendUserFromFixture(1);
 
         $dataMap = [
             'pages' => [
@@ -90,7 +88,6 @@ class PageTest extends FunctionalTestCase
         $this->importDataSet(__DIR__ . '/Fixtures/Database/pages.xml');
         $this->importDataSet(__DIR__ . '/Fixtures/Database/sys_language.xml');
         $this->importDataSet(__DIR__ . '/Fixtures/Database/pages_language_overlay.xml');
-        $this->setUpBackendUserFromFixture(1);
 
         $dataMap = [
             'pages_language_overlay' => [
