3c4c24fa5faa9adda0a60f71e61d3d7d2016cfa4
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-17 17:47:33 +0200

    [TASK] Prefix UID to page slugs

---
+++ Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,16 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA']['pages'], [
+    'columns' => [
+        'slug' => [
+            'config' => [
+                'generatorOptions' => [
+                    'fields' => [
+                        'uid',
+                        'title',
+                    ],
+                ],
+            ],
+        ],
+    ],
+]);
