431e9ea04f4063a6240d6241fe7142e9a52b9d7a
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-18 16:56:29 +0200

    [TASK] Add wrapper to iterate/count all pages

---
+++ Classes/Page/Page.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types = 1);
+
+namespace Pagemachine\FlatUrls\Page;
+
+final class Page
+{
+    /**
+     * @var int
+     */
+    private $uid;
+
+    public function __construct(int $uid)
+    {
+        $this->uid = $uid;
+    }
+
+    public function getUid(): int
+    {
+        return $this->uid;
+    }
+}
---
+++ Tests/Functional/Page/PageCollectionTest.php
@@ -0,0 +1,72 @@
+<?php
+declare(strict_types = 1);
+
+namespace Pagemachine\FlatUrls\Tests\Functional\Page;
+
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use Pagemachine\FlatUrls\Page\Page;
+use Pagemachine\FlatUrls\Page\PageCollection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Testcase for Pagemachine\FlatUrls\Page\PageCollection
+ */
+final class PageCollectionTest extends FunctionalTestCase
+{
+    /**
+     * @test
+     */
+    public function collectsAllPages(): void
+    {
+        $connection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('pages');
+
+        $connection->bulkInsert('pages',
+            [
+                [
+                    'uid' => 1,
+                    'title' => 'Regular page',
+                    'hidden' => 0,
+                    'deleted' => 0,
+                ],
+                [
+                    'uid' => 2,
+                    'title' => 'Deleted page',
+                    'hidden' => 0,
+                    'deleted' => 1,
+                ],
+                [
+                    'uid' => 3,
+                    'title' => 'Hidden page',
+                    'hidden' => 1,
+                    'deleted' => 0,
+                ],
+            ],
+            [
+                'uid',
+                'title',
+                'deleted',
+                'hidden',
+            ]
+        );
+
+        $pages = GeneralUtility::makeInstance(PageCollection::class);
+        $expected = [
+            [
+                'uid' => 1,
+            ],
+            [
+                'uid' => 3,
+            ],
+        ];
+
+        $this->assertCount(2, $pages);
+
+        $pagesList = iterator_to_array($pages);
+
+        $this->assertContainsOnlyInstancesOf(Page::class, $pagesList);
+        $this->assertEquals(1, $pagesList[0]->getUid());
+        $this->assertEquals(3, $pagesList[1]->getUid());
+    }
+}
