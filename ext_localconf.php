5927291e11715f6fd98938540f6a4cd6c8f674eb
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-21 11:14:23 +0200

    [TASK] Use array utility for code registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,18 +11,23 @@ defined('TYPO3_MODE') or die();
     );
 })();
 
-$GLOBALS['TYPO3_CONF_VARS']
-    ['SC_OPTIONS']
-        ['t3lib/class.t3lib_tcemain.php']
-            ['processDatamapClass']
-                [1600420737] = \Pagemachine\FlatUrls\Hook\DataHandlerHook::class;
-
-$GLOBALS['TYPO3_CONF_VARS']
-    ['SYS']
-        ['formEngine']
-            ['nodeRegistry']
-                [1600432195] = [
+\TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TYPO3_CONF_VARS'], [
+    'SC_OPTIONS' => [
+        't3lib/class.t3lib_tcemain.php' => [
+            'processDatamapClass' => [
+                1600420737 => \Pagemachine\FlatUrls\Hook\DataHandlerHook::class,
+            ],
+        ],
+    ],
+    'SYS' => [
+        'formEngine' => [
+            'nodeRegistry' => [
+                1600432195 => [
                     'nodeName' => 'staticSlug',
                     'priority' => 50,
                     'class' => \Pagemachine\FlatUrls\Backend\Form\Element\StaticSlugElement::class,
-                ];
+                ],
+            ],
+        ],
+    ],
+]);
