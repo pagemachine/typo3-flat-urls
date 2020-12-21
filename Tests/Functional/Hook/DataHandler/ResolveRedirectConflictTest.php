67d980b67ad3a1bbc78091ad27191707bf7c6099
Mathias Brodala <mbrodala@pagemachine.de>
2020-12-21 15:50:52 +0100

    [BUGFIX] Normalize page path in conflict detection
    We must ignore a trailing slash in generated page URIs for redirect
    conflict detection since redirects never have a trailing slash.
    
    A trailing slash can occur e.g. if a "PageType" route enhancer was added
    to a site.

--- Tests/Functional/Hook/DataHandler/ResolveRedirectConflictTest.php
+++ Tests/Functional/Hook/DataHandler/ResolveRedirectConflictTest.php
@@ -5,6 +5,8 @@ namespace Pagemachine\FlatUrls\Tests\Functional\Hook\DataHandler;
 
 use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Configuration\SiteConfiguration;
+use TYPO3\CMS\Core\Core\Environment;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -106,6 +108,85 @@ final class ResolveRedirectConflictTest extends FunctionalTestCase
                 'source_path' => '/foo',
                 'target' => 't3://page?uid=2',
             ],
+            [
+                'source_path' => '/2/second-title',
+                'target' => 't3://page?uid=2',
+            ],
+        ];
+
+        $this->assertArraySubset($expected, $redirects);
+    }
+
+    /**
+     * @test
+     */
+    public function normalizesPagePath(): void
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        $pageConnection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('pages');
+        $pageConnection->insert('pages', [
+            'uid' => 1,
+            'title' => 'Root',
+            'is_siteroot' => 1,
+        ]);
+        $this->setUpFrontendRootPage(1);
+
+        // Enforce trailing slash for generated page URIs
+        $siteConfiguration = GeneralUtility::makeInstance(SiteConfiguration::class, Environment::getConfigPath() . '/sites');
+        $siteConfigurationData = $siteConfiguration->load('1');
+        $siteConfigurationData['routeEnhancers'] = [
+            'pageTypeSuffix' => [
+                'type' => 'PageType',
+                'default' => '/',
+                'index' => '',
+                'map' => [
+                    '/' => 0,
+                ],
+            ],
+        ];
+        $siteConfiguration->write('1', $siteConfigurationData);
+
+        $pageConnection->insert('pages', [
+            'uid' => 2,
+            'pid' => 1,
+            'title' => 'First title',
+            'slug' => '/2/first-title',
+        ]);
+
+        $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+        $dataHandler->start([
+            'pages' => [
+                2 => [
+                    'title' => 'Second title',
+                ],
+            ],
+        ], []);
+        $dataHandler->process_datamap();
+
+        // Drop cached page used by PageRepository::getPage() through PageRouter::generateUri()
+        GeneralUtility::makeInstance(CacheManager::class)->flushCaches();
+
+        $dataHandler->start([
+            'pages' => [
+                2 => [
+                    'title' => 'First title',
+                ],
+            ],
+        ], []);
+        $dataHandler->process_datamap();
+
+        $redirectConnection = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getConnectionForTable('sys_redirect');
+
+        $this->assertEquals(1, $redirectConnection->count('*', 'sys_redirect', []));
+
+        $redirects = $redirectConnection
+            ->select(['source_path', 'target'], 'sys_redirect')
+            ->fetchAll();
+
+        $expected = [
             [
                 'source_path' => '/2/second-title',
                 'target' => 't3://page?uid=2',
